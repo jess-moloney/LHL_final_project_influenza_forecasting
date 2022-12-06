f2b63d6dc781464e2f6b16fb7f66ca980c05dda6
Jessica Moloney <80083302+jess-moloney@users.noreply.github.com>
2022-12-06 15:18:19 -0800

    work on forecast plot, 95% confidence intervals

--- src/evaluate_model.py
+++ src/evaluate_model.py
@@ -3,17 +3,17 @@ import pickle
 import pandas as pd
 import os.path
 
-def evaluate_model(train, test, preds_train, preds_test, dataset, forecast_weeks, model):
+def evaluate_model(train_y, test_y, preds_train, preds_test, dataset, forecast_weeks, model):
   
     # evaluate
-    MSE_train = round(mean_squared_error(train, preds_train),2)
-    MSE_test = round(mean_squared_error(test, preds_test),2)
-    RMSE_train = round(mean_squared_error(train, preds_train, squared=False),2)
-    RMSE_test = round(mean_squared_error(test, preds_test, squared=False),2)
-    MAE_train = round(mean_absolute_error(train, preds_train),2)
-    MAE_test = round(mean_absolute_error(test, preds_test),2)
-    R2_train = r2_score(train, preds_train)
-    R2_test = r2_score(test, preds_test)
+    MSE_train = round(mean_squared_error(train_y, preds_train),2)
+    MSE_test = round(mean_squared_error(test_y, preds_test),2)
+    RMSE_train = round(mean_squared_error(train_y, preds_train, squared=False),2)
+    RMSE_test = round(mean_squared_error(test_y, preds_test, squared=False),2)
+    MAE_train = round(mean_absolute_error(train_y, preds_train),2)
+    MAE_test = round(mean_absolute_error(test_y, preds_test),2)
+    R2_train = r2_score(train_y, preds_train)
+    R2_test = r2_score(test_y, preds_test)
 
     # MSE_train = round(mean_squared_error(train.iloc[:,forecast_weeks-1], preds_train[:,forecast_weeks-1]),2)
     # MSE_test = round(mean_squared_error(test.iloc[:,forecast_weeks-1], preds_test[:,forecast_weeks-1]),2)
